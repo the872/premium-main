cfe8e06a81be58c4a50d5dcab13b47f68487e43d
AID IDRIZOVIC <idrizovicaid@gmail.com>
2020-05-18 23:49:27 -0500

    more builds

--- build/sw.js
+++ build/sw.js
@@ -31,7 +31,7 @@ var __wpo = {
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "5/18/2020, 11:16:23 PM",
+  "version": "5/18/2020, 11:49:18 PM",
   "name": "webpack-offline",
   "pluginVersion": "5.0.5",
   "relativePaths": false
